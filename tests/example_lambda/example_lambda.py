59e246ce133dd35b8fb15d8642e8e0875e29579e
Yuki Wagatsuma <yuki-wagatsuma@mamezou.com>
2024-04-25 16:21:40 +0900

    Add chromium flags valid on AWS Lambda
    - ChromiumをAWS Lambda上で正常に動作させるために必要なフラグを環境変数 `PWPYLAMBDA_CHROMIUM_ARGS` として追加
    - `PWPYLAMBDA_CHROMIUM_ARGS` を利用するようにテストを修正
    - AWS Lambda向けのDockerfileを追加

--- tests/example_lambda/example_lambda.py
+++ tests/example_lambda/example_lambda.py
@@ -1,5 +1,6 @@
 """Playwrightを使ったLambda関数ハンドラのサンプル"""
 
+import os
 from typing import Any
 
 from playwright.sync_api import sync_playwright
@@ -18,8 +19,13 @@ def example_handler(event: dict[str, str], context: Any) -> str:
 
 
 def get_inner_text(url: str, css_selector: str) -> str:
+    args = os.getenv("PWPYLAMBDA_CHROMIUM_ARGS", "").split()
     with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
-        return page.locator(f"css={css_selector}").inner_text()
+        browser = p.chromium.launch(args=args)
+        try:
+            page = browser.new_page()
+            page.goto(url)
+            text = page.locator(f"css={css_selector}").inner_text()
+        finally:
+            browser.close()
+    return text
